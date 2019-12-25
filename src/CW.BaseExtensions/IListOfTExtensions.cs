37a6b2e6ba1777320f6171d456f3bb36f06f7940
catcherwong <catcher_hwq@outlook.com>
2019-12-25 22:50:14 +0800

    :sparkles: IList<T> extensions

---
+++ src/CW.BaseExtensions/IListOfTExtensions.cs
@@ -0,0 +1,71 @@
+﻿namespace System
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class IListOfTExtensions
+    {
+        /// <summary>
+        /// Indicates whether this list is null or empty.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="input">The list.</param>
+        /// <returns>true if null or empty, false if not.</returns>
+        public static bool IsNullOrEmpty<T>(this IList<T> input)
+            => input == null || !input.Any();
+
+        /// <summary>
+        ///  Indicates whether this list is not null or empty.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="input">The list.</param>
+        /// <returns>true if not null or empty, false if null or empty.</returns>
+        public static bool IsNotNullOrEmpty<T>(this IList<T> input)
+           => input != null && input.Any();
+
+        /// <summary>
+        /// Concatenates all the elements of this list, using the specified separator between each element.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="input">The list.</param>
+        /// <param name="separator">The separator.</param>
+        /// <returns> A string that consists of the elements in value delimited by the separator string.</returns>
+        public static string StringJoin<T>(this IEnumerable<T> input, string separator)
+            => string.Join(separator, input);
+
+        /// <summary>
+        /// Add a value if this list doesn't contains it already.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="input">The list.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>true if it succeeds, false if it fails.</returns>
+        public static bool AddIfNotContains<T>(this IList<T> input, T value)
+        {
+            if (!input.Contains(value))
+            {
+                input.Add(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Add a range of values to this list that's not already in the ICollection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="input">The list.</param>
+        /// <param name="values">The values.</param>
+        public static void AddRangeIfNotContains<T>(this IList<T> input, params T[] values)
+        {
+            foreach (T value in values)
+            {
+                if (!input.Contains(value))
+                {
+                    input.Add(value);
+                }
+            }
+        }
+    }
+}
---
+++ tests/CW.BaseExtensions.Tests/IListOfTExtensionsTest.cs
@@ -0,0 +1,91 @@
+﻿namespace CW.BaseExtensions.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Shouldly;
+    using Xunit;
+
+    public class IListOfTExtensionsTest
+    {
+        [Fact]
+        public void IsNullOrEmpty_Should_Succeed_When_Is_Empty()
+        {
+            var list = new List<int> { };
+
+            var res = list.IsNullOrEmpty();
+
+            res.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IsNullOrEmpty_Should_Succeed_When_Is_Null()
+        {
+            List<int> list = null;
+
+            var res = list.IsNullOrEmpty();
+
+            res.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void IsNotNullOrEmpty_Should_Succeed()
+        {
+            var list = new List<int> { 1 };
+
+            var res = list.IsNotNullOrEmpty();
+
+            res.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void StringJoin_Should_Succeed()
+        {
+            var list = new List<int> { 1, 2 };
+
+            var res = list.StringJoin(":");
+
+            res.ShouldBe("1:2");
+        }
+
+        [Fact]
+        public void StringJoin_Should_Succeed_When_Is_Empty()
+        {
+            var list = new List<int> { };
+
+            var res = list.StringJoin(":");
+
+            res.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void AddIfNotContains_Should_Return_False_When_Contains()
+        {
+            var list = new List<int> { 1 };
+
+            var res = list.AddIfNotContains(1);
+
+            res.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void AddIfNotContains_Should_Return_False_When_Not_Contains()
+        {
+            var list = new List<int> { 1 };
+
+            var res = list.AddIfNotContains(2);
+
+            res.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void AddRangeIfNotContains_Should()
+        {
+            var list = new List<int> { 1, 2, 3 };
+
+            list.AddRangeIfNotContains(new List<int> { 2, 3, 4 }.ToArray());
+
+            list.ShouldContain(4);
+            list.Count.ShouldBe(4);
+        }
+    }
+}
