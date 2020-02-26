7e8f810865b2c13bdecb37a9cc3c672429160c56
catcherwong <catcher_hwq@outlook.com>
2020-02-26 21:53:00 +0800

    :sparkles: dict extensions

---
+++ src/CW.BaseExtensions/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class DictionaryExtensions
+    {
+        public static string ToQueryString(this Dictionary<string, string> dict)
+        {
+            if (dict == null || !dict.Any()) return string.Empty;
+
+            List<string> list = new List<string>();
+
+            foreach (var item in dict)
+            {
+                list.Add($"{item.Key}={item.Value}");
+            }
+
+            return string.Join("&", list);
+        }
+    }
+}
---
+++ tests/CW.BaseExtensions.Tests/DictionaryExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿namespace CW.BaseExtensions.Tests
+{
+    using Shouldly;
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class DictionaryExtensionsTest
+    {
+        [Fact]
+        public void ToQueryString_Should_Return_Empty_When_Dict_IsEmpty()
+        {
+            var dict = new Dictionary<string, string>();
+
+            var queryString = dict.ToQueryString();
+
+            queryString.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void ToQueryString_Should_Return_Empty_When_Dict_IsNull()
+        {
+            Dictionary<string, string> dict = null;
+
+            var queryString = dict.ToQueryString();
+
+            queryString.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void ToQueryString_Should_Succeed()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "a", "b" },
+                { "c", "d" }
+            };
+
+            var queryString = dict.ToQueryString();
+
+            queryString.ShouldBe("a=b&c=d");
+        }
+    }
+}
